e488db5cd35e7f0de27073ac44fed3d6832cce3c
wsielyfeng <learnroad@163.com>
2021-04-01 11:28:42 +0800

    增加js引入、css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     mode: 'development',
     entry:'./src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     output: {
         filename:'[name].[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            title: 'My App',
+            template: 'src/assets/index.html'
+        }
+    )],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
     }
 };
+
+
+
